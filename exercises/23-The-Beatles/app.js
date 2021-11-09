461341fba8ba916898a146190fb3ec2cfc3cf5bb
Bruno Marques <besmarques@gmail.com>
2021-11-09 12:49:16 +0000

    update

--- exercises/23-The-Beatles/app.js
+++ exercises/23-The-Beatles/app.js
@@ -2,7 +2,7 @@ function sing(){
     var song = "";
     for (var i = 1; i <= 12; i++){
         if (i == 5 ){
-            song += "Whisper words of wisdom, ";
+            song += "whisper words of wisdom, ";
         }else if ( i == 11){
             song += "there will be an answer, ";
         } else if ( i == 12){
